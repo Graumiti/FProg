31fbd3e099246d36342607d51430a6df3cf320b0
Pedro <gouveiapedro141@gmail.com>
2025-03-28 09:23:28 +0000

    tpc 5.2

--- .venv/5.1.py
+++ .venv/5.1.py
@@ -8,7 +8,7 @@ def main():
     win.setBackground("white")
 
     radius = 20
-    x, y = win_width // 2, win_height // 2  # Posição inicial do círculo
+    x, y = win_width // 2, win_height // 2  # Posição inicial do círculo, a meio do ecrã
     dx, dy = 1, 1  # Velocidade do movimento
 
     circle = Circle(Point(x, y), radius)
@@ -20,19 +20,17 @@ def main():
         circle.move(dx, dy)
 
         center = circle.getCenter()
-        x, y = center.getX(), center.getY()
+        x, y = center.getX(), center.getY() #conforme o tempo, pega sempre a posição do centro do circulo.
 
-        # Verifica os limites da janela e inverte a direção se necessário
+        # Verifica a posição do circulo, relativamente a xx e yy e inverte a direção quando toca nos limites da janela
         if x - radius <= 0 or x + radius >= win_width:
             dx = -dx
         if y - radius <= 0 or y + radius >= win_height:
             dy = -dy
 
         if win.checkMouse():  # Permite encerrar ao clicar na janela
-            break
+            break # quebra do ciclo while, enquanto o utilizador não clicar na janela, o programa corre indefinitivamente.
 
     win.close()
 
-
-if __name__ == "__main__":
-    main()
+main()
--- .venv/5.2.py
+++ .venv/5.2.py
@@ -6,4 +6,4 @@ def contar_palavras(frase):
 frase = input("Digite uma frase: ")
 numero_palavras = contar_palavras(frase)
 
-print(f"A frase contém {numero_palavras} palavras.")
+print('A frase contém', numero_palavras, 'palavras.')
